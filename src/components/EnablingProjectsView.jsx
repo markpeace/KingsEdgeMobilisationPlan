a21d6194c22c18d2dce9e48fa61744e5f1d56d41
markpeace <mark@learnsomestuff.com>
2026-07-01 18:18:25 +0100

    Render simple related projects list

--- src/components/EnablingProjectsView.jsx
+++ src/components/EnablingProjectsView.jsx
@@ -1,5 +1,12 @@
 import { enablingProjects } from '../plan-utils.js';
 
 export default function EnablingProjectsView() {
-  return <main>{enablingProjects.length}</main>;
+  return (
+    <main>
+      <h1>Related projects</h1>
+      <ul>
+        {enablingProjects.map((project) => <li key={project.id}>{project.title}</li>)}
+      </ul>
+    </main>
+  );
 }
