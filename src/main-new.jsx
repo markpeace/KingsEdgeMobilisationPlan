0a08bd1411ca77bd0b4d30609df8bbac8eb0221c
markpeace <mark@learnsomestuff.com>
2026-07-01 18:20:10 +0100

    Import app in new entry

--- src/main-new.jsx
+++ src/main-new.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Application from './App.jsx';
 
-console.log(React, createRoot);
+console.log(React, createRoot, Application);
