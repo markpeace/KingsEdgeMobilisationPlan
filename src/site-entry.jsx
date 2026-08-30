6faa30ca1e9b2faf027179f460e6e0f231d19d39
markpeace <mark@learnsomestuff.com>
2026-08-30 18:44:17 +0100

    Migrate governance consultation and RAID styling

--- src/site-entry.jsx
+++ src/site-entry.jsx
@@ -2,5 +2,6 @@ import './site.jsx';
 import './styles/global-chrome.css';
 import './styles/detail-primitives.css';
 import './styles/deliverable-detail.css';
+import './styles/planning-detail.css';
 import './styles/project-overview.css';
 import './styles/post-legacy-cleanup.css';
